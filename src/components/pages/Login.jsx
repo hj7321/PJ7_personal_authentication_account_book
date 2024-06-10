707c67e09246ae71f4cf00c31e411e41730cfe3e
hj7321 <oboki2001@naver.com>
2024-06-11 08:18:14 +0900

    style: 로그인 페이지 스타일 구현

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,5 +1,31 @@
+import {
+  StSection,
+  StH3,
+  StIdPw,
+  StLoginBox,
+  StLabel,
+  StInput,
+  StButton,
+} from "../style/LoginStyle";
+
 const Login = () => {
-  return <div>로그인 페이지</div>;
+  return (
+    <StSection>
+      <StH3>로그인</StH3>
+      <StLoginBox>
+        <StIdPw>
+          <label>
+            아이디 <StInput type="text" />
+          </label>
+          <StLabel>
+            비밀번호 <StInput type="password" />
+          </StLabel>
+        </StIdPw>
+        <StButton $login>로그인</StButton>
+      </StLoginBox>
+      <StButton>회원가입 하기</StButton>
+    </StSection>
+  );
 };
 
 export default Login;
---
+++ src/components/style/LoginStyle.jsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+export const StButton = styled.button`
+  width: ${(props) => (props.$login ? "80px" : "130px")};
+  padding: 10px;
+  background-color: #fcd1d8;
+  border: 2px solid #fcd1d8;
+  border-radius: 5px;
+  font-size: 14px;
+  &:hover {
+    background-color: #fda0b0;
+    font-weight: bold;
+  }
+`;
+
+export const StSection = styled.section`
+  flex-direction: column;
+  text-align: center;
+  align-items: center;
+  padding: 40px;
+`;
+
+export const StH3 = styled.h3`
+  font-size: 25px;
+  padding-bottom: 20px;
+`;
+
+export const StIdPw = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+export const StLoginBox = styled.div`
+  display: flex;
+  gap: 10px;
+  margin: 30px;
+`;
+
+export const StLabel = styled.label`
+  margin-right: 16px;
+`;
+
+export const StInput = styled.input`
+  width: 200px;
+`;
