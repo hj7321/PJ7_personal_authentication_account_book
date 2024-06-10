f9e3eda4c5b4399ff90488937b948af0886de945
hj7321 <oboki2001@naver.com>
2024-06-10 17:45:54 +0900

    feat: 로그인, 회원가입 페이지 컴포넌트 라우터 설정

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "../components/pages/HomePage";
 import DetailPage from "../components/pages/DetailPage";
+import Login from "./../components/pages/Login";
+import SignUp from "../components/pages/SignUp";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
         <Route path="detail/:id" element={<DetailPage />} />
       </Routes>
     </BrowserRouter>
