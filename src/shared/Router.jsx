3115b6e04ccf26a3b4e889d75046d6487e979f21
hj7321 <oboki2001@naver.com>
2024-06-11 12:38:12 +0900

    fix: 헤더 네비게이션 바를 홈페이지, 디테일 페이지에서만 보이도록 라우터 수정

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -3,19 +3,19 @@ import HomePage from "../components/pages/HomePage";
 import DetailPage from "../components/pages/DetailPage";
 import Login from "./../components/pages/Login";
 import SignUp from "../components/pages/SignUp";
-import HeaderNav from "./HeaderNav";
+import HeaderNav from "../components/HeaderNav";
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <HeaderNav>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/detail/:id" element={<DetailPage />} />
-        </Routes>
-      </HeaderNav>
+      <Routes>
+        <Route path="/" element={<HeaderNav />}>
+          <Route index element={<HomePage />} />
+          <Route path="detail/:id" element={<DetailPage />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     </BrowserRouter>
   );
 };
