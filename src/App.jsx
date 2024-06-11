20147c85c4519655b4553488b638f6a4c0e24948
hj7321 <oboki2001@naver.com>
2024-06-11 20:52:43 +0900

    feat: 인증 컨텍스트 포함시킴

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import GlobalStyle from "./components/style/GlobalStyle";
+import { AuthProvider } from "./context/AuthContext";
 import Router from "./shared/Router";
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <GlobalStyle />
       <Router />
-    </>
+    </AuthProvider>
   );
 }
 
