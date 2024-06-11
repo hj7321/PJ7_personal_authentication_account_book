3adadb924fccaaa7caf4c86fbd62bc11f6c407e5
hj7321 <oboki2001@naver.com>
2024-06-11 20:51:32 +0900

    feat: 마이페이지 라우터 생성

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -4,6 +4,7 @@ import DetailPage from "../components/pages/DetailPage";
 import Login from "./../components/pages/Login";
 import SignUp from "../components/pages/SignUp";
 import HeaderNav from "../components/HeaderNav";
+import MyPage from "../components/pages/MyPage";
 
 const Router = () => {
   return (
@@ -11,6 +12,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<HeaderNav />}>
           <Route index element={<HomePage />} />
+          <Route path="profile" element={<MyPage />} />
           <Route path="detail/:id" element={<DetailPage />} />
         </Route>
         <Route path="/login" element={<Login />} />
