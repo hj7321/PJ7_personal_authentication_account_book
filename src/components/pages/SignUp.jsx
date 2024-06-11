012b041abfc25278b7dfa12b232bf3b4228b3a6a
hj7321 <oboki2001@naver.com>
2024-06-11 11:11:26 +0900

    feat: 회원가입 시 유효성 검사 기능 구현 & 로그인 버튼을 누르면 로그인 페이지로 이동하는 네비게이션 기능 구현

--- src/components/pages/SignUp.jsx
+++ src/components/pages/SignUp.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import {
   StSection,
   StH3,
@@ -8,27 +9,95 @@ import {
   StButton,
   StButtonBox,
 } from "../style/LoginStyle";
+import { useNavigate } from "react-router-dom";
+import { StMessage } from "../style/SignUpStyle";
 
 const SignUp = () => {
+  const [inputId, setInputId] = useState("");
+  const [inputPw, setInputPw] = useState("");
+  const [inputName, setInputName] = useState("");
+
+  const [idMsg, setIdMsg] = useState("");
+  const [pwMsg, setPwMsg] = useState("");
+  const [nameMsg, setNameMsg] = useState("");
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const validateId = () => {
+      if (inputId.length !== 0 && (inputId.length < 4 || inputId.length > 10))
+        setIdMsg("아이디는 4~10글자여야 합니다.");
+      else setIdMsg("");
+    };
+    validateId();
+  }, [inputId]);
+
+  useEffect(() => {
+    const validatePw = () => {
+      if (inputPw.length !== 0 && (inputPw.length < 4 || inputPw.length > 15))
+        setPwMsg("비밀번호는 4~15글자여야 합니다.");
+      else setPwMsg("");
+    };
+    validatePw();
+  }, [inputPw]);
+
+  useEffect(() => {
+    const validateName = () => {
+      if (
+        inputName.length !== 0 &&
+        (inputName.length < 1 || inputName.length > 10)
+      )
+        setNameMsg("닉네임은 1~10글자여야 합니다.");
+      else setNameMsg("");
+    };
+    validateName();
+  }, [inputName]);
+
   return (
     <StSection>
       <StH3>회원가입</StH3>
       <StLoginBox>
         <StIdPw>
           <label>
-            아이디 <StInput type="text" />
+            아이디{" "}
+            <StInput
+              type="text"
+              value={inputId}
+              onChange={(e) => setInputId(e.target.value)}
+            />
+            <StMessage $show={idMsg}>{idMsg}</StMessage>
           </label>
           <StLabel>
-            비밀번호 <StInput type="password" />
+            비밀번호{" "}
+            <StInput
+              type="password"
+              value={inputPw}
+              onChange={(e) => setInputPw(e.target.value)}
+            />
+            <StMessage $show={pwMsg} $pw>
+              {pwMsg}
+            </StMessage>
           </StLabel>
           <label>
-            닉네임 <StInput type="text" />
+            닉네임{" "}
+            <StInput
+              type="text"
+              value={inputName}
+              onChange={(e) => setInputName(e.target.value)}
+            />
+            <StMessage $show={nameMsg}>{nameMsg}</StMessage>
           </label>
         </StIdPw>
       </StLoginBox>
       <StButtonBox>
         <StButton $login>회원가입</StButton>
-        <StButton>로그인 하기</StButton>
+        <StButton
+          onClick={() => {
+            navigate("/login");
+          }}
+        >
+          로그인 하기
+        </StButton>
       </StButtonBox>
     </StSection>
   );
