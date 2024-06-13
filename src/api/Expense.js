db02e6b8b3f7e46b92b9a16dfd0696f48c6d0a31
hj7321 <oboki2001@naver.com>
2024-06-13 15:33:52 +0900

    feat: 서버에 있는 지출 내용을 수정/삭제하는 기능 구현

--- src/api/Expense.js
+++ src/api/Expense.js
@@ -13,3 +13,15 @@ export const getExpense = async (id) => {
 export const addExpense = (newExpense) => {
   return axios.post("http://localhost:5000/expenses", newExpense);
 };
+
+export const deleteExpense = async (id) => {
+  await axios.delete(`http://localhost:5000/expenses/${id}`);
+};
+
+export const updateExpense = async (updatedExpense) => {
+  const { data } = await axios.patch(
+    `http://localhost:5000/expenses/${updatedExpense.id}`,
+    updatedExpense
+  );
+  return data;
+};
