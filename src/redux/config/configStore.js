57f69f031bd4489c6ff07ff895c808a4e0b48bfd
hj7321 <oboki2001@naver.com>
2024-06-13 13:02:53 +0900

    delete: 필요 없는 파일 삭제

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import expenseSlice from "../slices/expenseSlice";
 import monthSlice from "../slices/monthSlice";
 
 const store = configureStore({
   reducer: {
-    expense: expenseSlice,
     month: monthSlice,
   },
 });
