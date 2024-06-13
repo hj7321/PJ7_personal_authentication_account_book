ebd7d998b248624f60398353775aa43a2e50c6b7
hj7321 <oboki2001@naver.com>
2024-06-13 09:50:28 +0900

    delete: 필요 없는 내용 삭제

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,4 +1,3 @@
-import axios from "axios";
 import { createContext, useState } from "react";
 
 export const AuthContext = createContext(null);
@@ -7,19 +6,6 @@ const token = localStorage.getItem("accessToken");
 
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(!!token);
-
-  // const getNickname = async () => {
-  //   const { data } = await axios.get(
-  //     "https://moneyfulpublicpolicy.co.kr/user",
-  //     {
-  //       headers: {
-  //         "Content-type": "application/json",
-  //         Authorization: `Bearer ${token}`,
-  //       },
-  //     }
-  //   );
-  //   return data.nickname;
-  // };
 
   const login = (token) => {
     localStorage.setItem("accessToken", token);
