de88f27f268509621396f0437aff97c10365c535
hj7321 <oboki2001@naver.com>
2024-06-13 15:32:40 +0900

    feat: useQuery 훅을 통해 특정 지출 내용을 가져오고, useMutation 훅을 통해 서버에 지출 내용을 수정/삭제하는 기능 구현

--- src/components/detail/DetailContent.jsx
+++ src/components/detail/DetailContent.jsx
@@ -3,49 +3,66 @@ import { useEffect, useRef } from "react";
 import DetailInput from "./DetailInput";
 import { StButton, StDiv } from "../style/DetailContentStyle";
 import validateInput from "../../shared/validateInput";
-import { useDispatch, useSelector } from "react-redux";
-import { deleteExpense, updateExpense } from "../../redux/slices/expenseSlice";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { deleteExpense, getExpense, updateExpense } from "../../api/Expense";
 
 const DetailContent = () => {
-  const { expense } = useSelector((state) => state.expense);
-
-  const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const param = useParams();
-  const clickedExpense = expense.find((obj) => obj.id === param.id);
+  const { id } = useParams();
 
-  const date = useRef(clickedExpense.date);
-  const item = useRef(clickedExpense.item);
-  const amount = useRef(clickedExpense.amount);
-  const description = useRef(clickedExpense.description);
+  const { data: clickedExpense, isLoading } = useQuery({
+    queryKey: ["expense", id],
+    queryFn: () => getExpense(id),
+  });
 
-  const onUpdateHandler = (id) => {
+  const date = useRef(null);
+  const item = useRef(null);
+  const amount = useRef(null);
+  const description = useRef(null);
+
+  const queryClient = useQueryClient();
+
+  const deleteMutation = useMutation({
+    mutationFn: deleteExpense,
+    onSuccess: () => {
+      queryClient.invalidateQueries(["expenses"]);
+    },
+  });
+
+  const updateMutation = useMutation({
+    mutationFn: updateExpense,
+    onSettled: (updatedExpense) => {
+      queryClient.invalidateQueries(["expenses", updatedExpense.id]);
+    },
+  });
+
+  const onUpdateHandler = () => {
     if (
       validateInput(
         date.current.value,
         item.current.value,
-        amount.current.value,
+        +amount.current.value,
         description.current.value
       )
     ) {
-      dispatch(
-        updateExpense({
-          id,
-          date: date.current.value,
-          item: item.current.value,
-          amount: amount.current.value,
-          description: description.current.value,
-        })
-      );
+      updateMutation.mutate({
+        ...clickedExpense,
+        date: date.current.value,
+        item: item.current.value,
+        amount: +amount.current.value,
+        description: description.current.value,
+      });
+      alert("내역이 수정되었습니다.");
       navigate("/");
     }
   };
 
-  const onDeleteHandler = (id) => {
+  const onDeleteHandler = () => {
     const result = confirm("정말로 이 지출 내역을 삭제하시겠습니까?");
     if (result) {
-      dispatch(deleteExpense({ id }));
+      deleteMutation.mutate(clickedExpense.id);
+      alert("내역이 삭제되었습니다.");
       navigate("/");
     }
   };
@@ -55,27 +72,27 @@ const DetailContent = () => {
   };
 
   useEffect(() => {
-    date.current.value = clickedExpense.date;
-    item.current.value = clickedExpense.item;
-    amount.current.value = clickedExpense.amount;
-    description.current.value = clickedExpense.description;
-  }, []);
+    date.current.value = clickedExpense?.date;
+    item.current.value = clickedExpense?.item;
+    amount.current.value = clickedExpense?.amount;
+    description.current.value = clickedExpense?.description;
+  }, [clickedExpense]);
 
   return (
     <>
-      <DetailInput engName={"date"} korName={"날짜"} state={date} />
-      <DetailInput engName={"item"} korName={"항목"} state={item} />
-      <DetailInput engName={"amount"} korName={"금액"} state={amount} />
+      <DetailInput engName={"date"} korName={"날짜"} value={date} />
+      <DetailInput engName={"item"} korName={"항목"} value={item} />
+      <DetailInput engName={"amount"} korName={"금액"} value={amount} />
       <DetailInput
         engName={"description"}
         korName={"내용"}
-        state={description}
+        value={description}
       />
       <StDiv>
-        <StButton $color={"blue"} onClick={() => onUpdateHandler(param.id)}>
+        <StButton $color={"blue"} onClick={onUpdateHandler}>
           수정
         </StButton>
-        <StButton $color={"red"} onClick={() => onDeleteHandler(param.id)}>
+        <StButton $color={"red"} onClick={onDeleteHandler}>
           삭제
         </StButton>
         <StButton $color={"green"} onClick={gotoHomePage}>
