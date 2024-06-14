e78c2ba502bad91cd4bea303381714d7de33369c
hj7321 <oboki2001@naver.com>
2024-06-14 12:52:39 +0900

    refactor: month와 setMonth를 사용하는 컴포넌트의 공통 컴포넌트인 HomePage 컴포넌트에 상태 선언 및 props-drilling을 통해 상태와 함수를 넘겨주는 것으로 리팩토링(이유: depth가 1임)

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -2,14 +2,19 @@ import Form from "../home/Form";
 import Calendar from "../home/Calendar";
 import Graph from "../home/Graph";
 import Category from "../home/Category";
+import { useState } from "react";
 
 const HomePage = () => {
+  const [month, setMonth] = useState(
+    JSON.parse(localStorage.getItem("month")) || 1
+  );
+
   return (
     <>
       <Form />
-      <Calendar />
-      <Graph />
-      <Category />
+      <Calendar month={month} setMonth={setMonth} />
+      <Graph month={month} />
+      <Category month={month} />
     </>
   );
 };
