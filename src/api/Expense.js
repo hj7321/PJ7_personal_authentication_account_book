d7977aad972736d06321f57b431ebe2dd1aa9b1f
hj7321 <oboki2001@naver.com>
2024-06-14 13:20:06 +0900

    fix + delete: json server 주소 변경 & 필요 없는 db 데이터 삭제

--- src/api/Expense.js
+++ src/api/Expense.js
@@ -1,26 +1,28 @@
 import axios from "axios";
 
+const JSON_SERVER_HOST = "https://wise-amber-shovel.glitch.me";
+
 export const getExpenses = async () => {
-  const { data } = await axios.get("http://localhost:5000/expenses");
+  const { data } = await axios.get(`${JSON_SERVER_HOST}/expenses`);
   return data;
 };
 
 export const getExpense = async (id) => {
-  const { data } = await axios.get(`http://localhost:5000/expenses/${id}`);
+  const { data } = await axios.get(`${JSON_SERVER_HOST}/expenses/${id}`);
   return data;
 };
 
 export const addExpense = (newExpense) => {
-  return axios.post("http://localhost:5000/expenses", newExpense);
+  return axios.post(`${JSON_SERVER_HOST}/expenses`, newExpense);
 };
 
 export const deleteExpense = async (id) => {
-  await axios.delete(`http://localhost:5000/expenses/${id}`);
+  await axios.delete(`${JSON_SERVER_HOST}/expenses/${id}`);
 };
 
 export const updateExpense = async (updatedExpense) => {
   const { data } = await axios.patch(
-    `http://localhost:5000/expenses/${updatedExpense.id}`,
+    `${JSON_SERVER_HOST}/expenses/${updatedExpense.id}`,
     updatedExpense
   );
   return data;
