40e8926a3bc6df7fef74b54b6512d9211abdc49f
hj7321 <oboki2001@naver.com>
2024-06-14 12:39:55 +0900

    delete: 필요 없는 폴더/파일 삭제 및 필요 없는 import 문 삭제

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { Provider } from "react-redux";
-import store from "./redux/config/configStore.js";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
@@ -10,9 +8,7 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </QueryClientProvider>
   </React.StrictMode>
 );
